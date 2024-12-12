c5fd658ddc62b2041b267726afa4dc1b32feb18f
Logan <lmaier@uwaterloo.ca>
2024-12-12 02:25:42 -0500

    day 6 part 2 (check all positions if it loops, [using huristic of only needing to check the places that the guard will walk])

--- day6.py
+++ day6.py
@@ -27,8 +27,6 @@ def part1():
             total += 1
             visited.add((curx, cury))
 
-        #print(curx, cury)
-
         match dir:
             case 'N':
                 if cury == 0:
@@ -65,4 +63,126 @@ def part1():
     print(total)
 
 
+def part2():
+    total = 0
+
+    arr = []
+
+    curx, cury = 0, 0
+    dir = 'N'
+
+    y = 0
+    f = open("day6/day6_input.txt", "r")
+    for line in f:
+        l = list(line.strip())
+
+        if l.count('^') > 0:
+            curx = l.index('^')
+            cury = y
+
+        y += 1
+        arr.append(l)
+
+    f.close()
+
+    orgx, orgy = curx, cury
+
+    visited = set()
+
+    while True:
+        if (curx, cury) not in visited:
+            visited.add((curx, cury))
+
+        match dir:
+            case 'N':
+                if cury == 0:
+                    break
+                elif arr[cury - 1][curx] == "#":
+                    dir = 'E'
+                else:
+                    cury -= 1
+
+            case 'E':
+                if curx == len(arr[0]) - 1:
+                    break
+                elif arr[cury][curx + 1] == "#":
+                    dir = 'S'
+                else:
+                    curx += 1
+
+            case 'S':
+                if cury == len(arr) - 1:
+                    break
+                elif arr[cury + 1][curx] == "#":
+                    dir = 'W'
+                else:
+                    cury += 1
+
+            case 'W':
+                if curx == 0:
+                    break
+                elif arr[cury][curx - 1] == "#":
+                    dir = 'N'
+                else:
+                    curx -= 1
+
+
+    lasty, lastx = None, None
+    for item in visited:
+
+        if lastx is not None:
+            arr[lasty][lastx] = '.'
+        
+        curx, cury = orgx, orgy
+        dir = 'N'
+        v = set()
+
+        arr[item[1]][item[0]] = '#'
+        lasty, lastx = item[1], item[0]
+
+        while True:
+            if (curx, cury, dir) not in v:
+                v.add((curx, cury, dir))
+            else:
+                total += 1
+                break
+
+            match dir:
+                case 'N':
+                    if cury == 0:
+                        break
+                    elif arr[cury - 1][curx] == "#":
+                        dir = 'E'
+                    else:
+                        cury -= 1
+
+                case 'E':
+                    if curx == len(arr[0]) - 1:
+                        break
+                    elif arr[cury][curx + 1] == "#":
+                        dir = 'S'
+                    else:
+                        curx += 1
+
+                case 'S':
+                    if cury == len(arr) - 1:
+                        break
+                    elif arr[cury + 1][curx] == "#":
+                        dir = 'W'
+                    else:
+                        cury += 1
+
+                case 'W':
+                    if curx == 0:
+                        break
+                    elif arr[cury][curx - 1] == "#":
+                        dir = 'N'
+                    else:
+                        curx -= 1
+    
+
+    print(total)
+
+
 part1()
+part2()
